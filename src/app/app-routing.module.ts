8bc9810bd84f198a625e74f9179c5682f50ac18d
Mandla Mthethwa <mthethwa@gmail.com>
2022-07-08 08:29:26 +0200

    sentence changes08072022

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SentenceListComponent } from './components/sentence-list/sentence-list.component';
-import { SentenceDetailsComponent } from './components/sentence-details/sentence-details.component';
 import { AddTypesComponent } from './components/add-types/add-types.component';
 
 
 const routes: Routes = [
-  { path: '', redirectTo: 'senteces', pathMatch: 'full'},
+  { path: '', redirectTo: 'sentences', pathMatch: 'full'},
   { path: 'sentences', component: SentenceListComponent},
-  { path: 'sentences/:id', component: SentenceDetailsComponent},
   { path: 'add', component: AddTypesComponent }
 ];
 
--- src/app/components/sentence-list/sentence-list.component.ts
+++ src/app/components/sentence-list/sentence-list.component.ts
@@ -1,12 +1,19 @@
-import { Component, OnInit, EventEmitter } from '@angular/core';
+import { Component, OnInit, EventEmitter, OnChanges } from '@angular/core';
 import {Sentence} from 'src/app/models/sentence.model';
 import { Types} from 'src/app/models/types.model';
 import { SentenceService} from 'src/app/services/sentence.service';
 import { SentenceTypeService} from 'src/app/services/sentence-type.service';
-
-import { Noun} from 'src/app/models/noun.model';
-
 import { NounService} from 'src/app/services/noun.service';
+import { VerbService} from 'src/app/services/verb.service';
+import { AdverbService} from 'src/app/services/adverb.service';
+import { AdjectiveService} from 'src/app/services/adjective.service';
+import { PrepositionService} from 'src/app/services/preposition.service';
+import { PronounService} from 'src/app/services/pronoun.service';
+import { ConjuctionService} from 'src/app/services/conjuction.service';
+import { DeterminerService} from 'src/app/services/determiner.service';
+import { ExclamationService} from 'src/app/services/exclamation.service';
+
+
 @Component({
   selector: 'app-sentence-list',
   templateUrl: './sentence-list.component.html',
@@ -17,45 +24,37 @@ export class SentenceListComponent implements OnInit {
   currentSentence: Sentence={};
   currentIndex = -1;
   description ='';
+  newSenType ='';
+  newSenWord='';
   types?: Types[];
   inprogress = [''];
-  nouns: Noun={noun:''};
-  currentList?: Noun[];
+  baseList: any[''];
+  status: boolean;
+  resMessage: any;
+  sText: string;
 
-  constructor(private sentenceService: SentenceService, private TypeService: SentenceTypeService, private nounService: NounService) { }
+  constructor(private sentenceService: SentenceService, private TypeService: SentenceTypeService, private nounService: NounService,
+              private verbService: VerbService, private adverbService: AdverbService, private adjectiveService: AdjectiveService,
+              private prepositionService: PrepositionService, private pronounService: PronounService, private conjunctionService: ConjuctionService,
+              private determinerService: DeterminerService, private exclamationService: ExclamationService) { 
+                this.status=false;
+                this.sText='';
+              }
 
   ngOnInit(): void {
-    this.retrieveSentences();
-    this.retrieveTypes();
+      this.retrieveSentences();
+      this.retrieveTypes();
+      this.status=true;    
   }
   retrieveSentences(): void{
     this.sentenceService.getAll()
       .subscribe({
         next: (data: any) => {
           this.sentences = data;
-          console.log("list of sentence: "+ data);
         },
         error: (e: any) => console.error(e)
       });
-  }
-  refreshList(): void {
-    this.retrieveSentences();
-    this.currentSentence = {};
-    this.currentIndex = -1;
-  }
-  setActiveSentence(sentence: Sentence, index: number): void {
-    this.currentSentence = sentence;
-    this.currentIndex = index;
-  }
-  removeSentence(): void {
-    this.sentenceService.delete(this.description)
-      .subscribe({
-        next: (res: any) => {
-          this.refreshList();
-        },
-          error:(e:any) => console.error(e)
-      });
-  }
+  }  
   retrieveTypes(): void{
     this.TypeService.getAll()
       .subscribe({
@@ -66,24 +65,86 @@ export class SentenceListComponent implements OnInit {
       });
   }
   updateNewWord(incoming: string){
+    this.newSenType = incoming;
+    this.status=false; 
     if(incoming){
       if(incoming==="Noun"){
-        this.getAllNouns();
+        this.nounService.getAll()
+          .subscribe({
+            next: (data: any) => {
+              this.baseList = data;
+            },
+            error: (e: any) => console.error(e)
+          });
       }else if(incoming ==="Verb"){
-      
+        this.verbService.getAll()
+          .subscribe({
+            next: (data: any) => {
+              this.baseList = data;
+            },
+            error: (e: any) => console.error(e)
+          });
       }else if(incoming ==="Adverb"){
-
+        this.adverbService.getAll()
+          .subscribe({
+            next: (data: any) => {
+              this.baseList = data;
+            },
+            error: (e: any) => console.error(e)
+          });
       }else if(incoming ==="Adjective"){
+        this.adjectiveService.getAll()
+          .subscribe({
+            next: (data: any) => {
+              this.baseList = data;
+            },
+            error: (e: any) => console.error(e)
+          });
 
       }else if(incoming ==="Pronoun"){
+        this.pronounService.getAll()
+          .subscribe({
+            next: (data: any) => {
+              this.baseList = data;
+            },
+            error: (e: any) => console.error(e)
+          });
 
       }else if(incoming ==="Preposition"){
+        this.prepositionService.getAll()
+          .subscribe({
+            next: (data: any) => {
+              this.baseList = data;
+            },
+            error: (e: any) => console.error(e)
+          });
 
       }else if(incoming ==="Conjunction"){
+        this.conjunctionService.getAll()
+          .subscribe({
+            next: (data: any) => {
+              this.baseList = data;
+            },
+            error: (e: any) => console.error(e)
+          });
 
       }else if(incoming ==="Determiner"){
+        this.determinerService.getAll()
+          .subscribe({
+            next: (data: any) => {
+              this.baseList = data;
+            },
+            error: (e: any) => console.error(e)
+          });
 
       }else if(incoming ==="Exclamation"){
+        this.exclamationService.getAll()
+          .subscribe({
+            next: (data: any) => {
+              this.baseList = data;
+            },
+            error: (e: any) => console.error(e)
+          });
       
       }else{
         console.log("Not known word type!");      
@@ -91,18 +152,22 @@ export class SentenceListComponent implements OnInit {
     }
   }
   updateSelection(newSelection: string){
+    this.newSenType = newSelection;
     if (newSelection){
-      this.inprogress.push(newSelection);      
+      this.inprogress.push(newSelection);    
+      this.status=true; 
     }
+    this.ngOnInit();
   }
-  getAllNouns(): void{
-    this.nounService.getAll()
-    .subscribe({
-      next: (data: any) => {
-        this.currentList = data;
-        console.log("list of nouns: "+ this.currentList);
-      },
-      error: (e: any) => console.error(e)
-    });
+  commitSentence(){
+    this.sText = this.inprogress.toString();
+    this.sText = this.sText.replace(/[^a-zA-Z ]/g, ' ');
+    this.sentenceService.create({ "description": this.sText })
+      .subscribe({
+        next: (res) => {
+          this.resMessage = "successfully added";
+        },
+        error: (e) => console.error(e)
+      });
   }
 }
